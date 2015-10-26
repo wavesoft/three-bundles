e31394a84663e45ff531919063f09e6297efaeae
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-10-26 01:07:18 +0100

    Removing unused object and sound plugins

--- js/main.js
+++ js/main.js
@@ -10,11 +10,9 @@ define(function() {
 				'bundle'	 : 'three-bundles/plugin/bundle',
 				'material'	 : 'three-bundles/plugin/material',
 				'mesh'		 : 'three-bundles/plugin/mesh',
-				'object'	 : 'three-bundles/plugin/object',
 				'geometry'	 : 'three-bundles/plugin/geometry',
 				'scene'		 : 'three-bundles/plugin/scene',
 				'shader'	 : 'three-bundles/plugin/shader',
-				'sound'		 : 'three-bundles/plugin/texture',
 				'texture'	 : 'three-bundles/plugin/texture',
 			}
 		}

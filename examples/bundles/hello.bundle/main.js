25ce2827c1e24861ec4ea27d063daa62d8555049
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-11-02 00:17:51 +0100

    Fixing entry point of hello.bundle

--- examples/bundles/hello.bundle/main.js
+++ examples/bundles/hello.bundle/main.js
@@ -1,17 +1,12 @@
 /**
  * That's the bundles entry point
  */
-define(["three", "mesh!./monster.json"], function(THREE, monster) {
+define(["three", "mesh!./monster.json", "mesh!./utf8/ben_dds.utf8_js"], function(THREE, monster, ben) {
 
-	// Create an object to contain the meshes
-	var container = new THREE.Object3D();
-
-	// Plase the man model in the container
-	monster.scale.set( 0.5, 0.5, 0.5 );
-	monster.position.set( -300, 0, 0 );
-	container.add( monster );
-
-	// Return the container object
-	return container;
+	// Return monster and ben
+	return {
+		'monster': monster,
+		'ben': ben
+	};
 
 });
